f3d8fb7db46706d9c0d3a201ac4023ccd6fecc83
S44nt1ag0 <santiago.develop1@gmail.com>
2025-09-06 16:56:26 -0300

    added fundById repository

--- src/Interfaces/IUserRepository.ts
+++ src/Interfaces/IUserRepository.ts
@@ -2,6 +2,7 @@ import IUser from "../Entities/User";
 
 export default interface IUserRepository {
   findPasswordByEmail(email: string): Promise<string>;
+  findById(id: string): Promise<IUser | null>;
   create(user: IUser): Promise<IUser>;
   findByEmail(email: string): Promise<IUser | null>;
 }
