779f52d2fe591c0fa6d19f20bb0ec479fd44f078
S44nt1ag0 <santiago.develop1@gmail.com>
2025-09-07 19:07:37 -0300

    fix structure router /me

--- src/Controllers/Users/MeUser.ts
+++ src/Controllers/Users/MeUser.ts
@@ -22,7 +22,7 @@ class MeUser {
         return res.status(404).json({ error: "User not found." });
       }
 
-      return res.status(200).json({ user: UserDTO.basicData(user) });
+      return res.status(200).json({ ...UserDTO.basicData(user) });
     } catch (error) {
       console.log(error);
       return res.status(500).json({ error: "Error to fetch user data." });
