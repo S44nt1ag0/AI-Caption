e9524436b91b42ffa71fbe053d9ad0251032266e
S44nt1ag0 <santiago.develop1@gmail.com>
2025-09-13 21:28:41 -0300

    create instance of GoogleGenAI

---
+++ src/Services/GeminiService.ts
@@ -0,0 +1,19 @@
+import { GoogleGenAI } from "@google/genai";
+import { IGemini } from "../Interfaces/IGemini";
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GOOGLE_API_KEY,
+});
+
+export async function GeminiApi(body: string): Promise<IGemini> {
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: `summarize this ${body}`,
+    });
+
+    return { text: response?.text };
+  } catch {
+    return { error: "Error gemini summary." };
+  }
+}
