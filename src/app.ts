852dbbc02699ff6764f1fa2570c78433c73113b5
S44nt1ag0 <santiago.develop1@gmail.com>
2025-09-22 20:32:20 -0300

    added corret url frontEnd

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ try {
 
 app.use(
   cors({
-    origin: "https://api.captionai.lat",
+    origin: "https://www.captionai.lat",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
