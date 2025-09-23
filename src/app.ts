a7620d8b2af5fee12a1aa3d53552c89480e69e42
S44nt1ag0 <santiago.develop1@gmail.com>
2025-09-22 21:01:00 -0300

    accept all urls cors

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ try {
 
 app.use(
   cors({
-    origin: "https://www.captionai.lat",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
